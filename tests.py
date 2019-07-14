dc4b3c58cdda4578cc517ca417a1c949228bd2b8
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-14 18:42:33 -0400

    print output on failure in testing code

--- tests.py
+++ tests.py
@@ -1,23 +1,25 @@
 import requests
+from pprint import pprint as print
+
 url = 'http://127.0.0.1:5000/'
+output = {}
 
-assert (requests.get(url, params={'file': 'test_functions',
-                                 'function': 'add',
-                                 'a': 5,
-                                 'b': 15}).json()["return"] 
+try:
+    output = (requests.get(url, params={'file': 'test_functions',
+                                     'function': 'add',
+                                     'a': 5,
+                                     'b': 15}).json())
+    assert output["return"] == 20
 
-                                 == 20)
+    output = (requests.get(url, params={'file': 'test_functions',
+                                     'function': 'add',
+                                     'a': '\"5\"',
+                                     'b': '\"15\"'}).json())
+    assert output["return"] == "515"
 
-assert (requests.get(url, params={'file': 'test_functions',
-                                 'function': 'add',
-                                 'a': '\"5\"',
-                                 'b': '\"15\"'}).json()["return"] 
-
-                                 == "515")
-
-assert (requests.get(url, params={'file': 'test_functions',
-                                 'function': 'type_of',
-                                 'arg': '\"ababa\"'}).json()["return"]
-                                 
-                                 == "<class \'str\'>")
-
+    output = (requests.get(url, params={'file': 'test_functions',
+                                     'function': 'type_of',
+                                     'arg': '\"ababa\"'}).json())
+    assert output["return"] == "<class \'str\'>"
+except AssertionError:
+    print(output)
