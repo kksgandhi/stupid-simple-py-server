5e7ace214ffb05b548909853141131b881716a6a
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-14 18:33:51 -0400

    add code to test server itself

--- tests.py
+++ tests.py
@@ -1,1 +1,23 @@
+import requests
+url = 'http://127.0.0.1:5000/'
+
+assert (requests.get(url, params={'file': 'test_functions',
+                                 'function': 'add',
+                                 'a': 5,
+                                 'b': 15}).json()["return"] 
+
+                                 == 20)
+
+assert (requests.get(url, params={'file': 'test_functions',
+                                 'function': 'add',
+                                 'a': '\"5\"',
+                                 'b': '\"15\"'}).json()["return"] 
+
+                                 == "515")
+
+assert (requests.get(url, params={'file': 'test_functions',
+                                 'function': 'type_of',
+                                 'arg': '\"ababa\"'}).json()["return"]
+                                 
+                                 == "<class \'str\'>")
 
